4e7ff6285d354d70e98313f3e4a5899aa88a0cc1
Yahya Fati <yfati037@gmail.com>
2022-04-03 13:55:17 +0300

    fixed header files

--- rush.c
+++ rush.c
@@ -10,30 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "second.h"
+#include "third.h"
 
-struct s_valid_check {
-	int	col_vals[5];
-	int	view_count[4];
-	int	maxes[4];
-	int	items[4];
+struct s_valid_check
+{
+	int col_vals[5];   // Column Data
+	int view_count[4]; // Number of buildings seen from eachsides
+	int maxes[4];	   // Height of tallest building seen from eachsides
+	int items[4];	   // current buildings seen from eachsides
 };
 
 /**
-* argv -> a string containaing the list of number given by the user.
-* views[][] -> a 2 dimensional array where the users input is copied into.
-*			-> it is a 4 x 4 table (4 rows, 4 cols)
-*			-> each row - contains the view from one side
-*				row[0] -> view from top
-*				row[1] -> view from bottom
-*				row[2] -> view from left
-*				row[3] -> view from right
-*/
-int	initialize_array(int views[4][4], char *argv)
+ * argv -> a string containing the list of number given by the user.
+ * views[][] -> a 2 dimensional array where the users input is copied into.
+ *			-> it is a 4 x 4 table (4 rows, 4 cols)
+ *			-> each row - contains the view from one side
+ *				row[0] -> view from top
+ *				row[1] -> view from bottom
+ *				row[2] -> view from left
+ *				row[3] -> view from right
+ */
+int initialize_array(int views[4][4], char *argv)
 {
-	int	i;
-	int	j;
-	int	len;
+	int i;
+	int j;
+	int len;
 
 	i = 0;
 	len = 0;
@@ -70,12 +73,15 @@ Checks for two things:
 	2. are the views valid from every perspective
 		do they satisfy the input given by the user?
 If at least one of them is false, then it is not valid, otherwise it is valid.
-*/ 
-int	is_valid(int res[4][4], int given[4][4], int i, int j)
+*/
+int is_valid(int res[4][4], int given[4][4])
 {
-	struct s_valid_check	checker;
+	struct s_valid_check checker;
+	int i;
+	int j;
 
 	zeroize(checker.col_vals, 5);
+	i = 0;
 	while (i < 4)
 	{
 		zeroize(checker.view_count, 4);
@@ -83,14 +89,15 @@ int	is_valid(int res[4][4], int given[4][4], int i, int j)
 		j = 0;
 		while (j < 4)
 		{
+			// Gets the building seen from eachside and puts it into items[4]
 			set_items(res, checker.items, i, j);
-			if (checker.items[0]
-				&& is_col_taken(checker.col_vals[checker.items[0]], i))
+			if (checker.items[0] && is_col_taken(checker.col_vals[checker.items[0]], i))
 				return (0);
 			checker.col_vals[checker.items[0]] += (1 << i);
 			set_values(checker.view_count, checker.maxes, checker.items);
 			j++;
 		}
+		// Checks if it fits the constraints given by the user.
 		if (check_constraints(given, checker.view_count, i))
 			return (0);
 		i++;
@@ -100,7 +107,7 @@ int	is_valid(int res[4][4], int given[4][4], int i, int j)
 
 /*
 	Check the solve_stuff() function first.
-	
+
 	Continuation of solve_stuff() function, because of norminette.
 
 	Because of norminette, this changes only the first two numbers in the list
@@ -111,29 +118,31 @@ int	is_valid(int res[4][4], int given[4][4], int i, int j)
 		{4, 1, 2, 3}, this is also managed by this function
 	}
 */
-int	last_two_values(int res[4][4], int given[4][4],
-int *indexes, const int *values)
-{	
-	indexes[2] = 0;
-	while (indexes[2] < 24)
+int last_two_values(int res[4][4], int given[4][4], const int *values)
+{
+	int k;
+	int m;
+
+	k = 0;
+	while (k < 24)
 	{
-		to_int_array(res[2], values[indexes[2]]);
-		indexes[3] = 0;
-		while (indexes[3] < 24)
+		to_int_array(res[2], values[k]);
+		m = 0;
+		while (m < 24)
 		{
-			to_int_array(res[3], values[indexes[3]]);
-			if (is_valid(res, given, 0, 0))
+			to_int_array(res[3], values[m]);
+			if (is_valid(res, given))
 				return (1);
-			indexes[3]++;
+			m++;
 		}
-		indexes[2]++;
+		k++;
 	}
 	return (0);
 }
 
 /*
 	Solves the problem.
-	
+
 	It will try all possible combinations, and if it finds one that works,
 	then it will return 1 - meaning it has succefully solved it.
 
@@ -148,26 +157,26 @@ int *indexes, const int *values)
 		{4, 1, 2, 3}, this is also managed by last_two_values() function
 	}
 */
-int	solve_stuff(int res[4][4], int given[4][4])
+int solve_stuff(int res[4][4], int given[4][4])
 {
-	const int	values[] = {1234, 1243, 1324, 1342, 1423, 1432, 2134,
-		2143, 2314, 2341, 2413, 2431, 3124, 3142, 3214,
-		3241, 3412, 3421, 4123, 4132, 4213, 4231, 4312, 4321};
-	int			indexes[4];
+	const int values[] = {1234, 1243, 1324, 1342, 1423, 1432, 2134,
+						  2143, 2314, 2341, 2413, 2431, 3124, 3142, 3214,
+						  3241, 3412, 3421, 4123, 4132, 4213, 4231, 4312, 4321};
+	int i, j;
 
-	zeroize(indexes, 4);
-	while (indexes[0] < 24)
+	i = 0;
+	while (i < 24)
 	{
-		to_int_array(res[0], values[indexes[0]]);
-		indexes[1] = 0;
-		while (indexes[1] < 24)
+		to_int_array(res[0], values[i]);
+		j = 0;
+		while (j < 24)
 		{
-			to_int_array(res[1], values[indexes[1]]);
-			if (last_two_values(res, given, indexes, values))
+			to_int_array(res[1], values[j]);
+			if (last_two_values(res, given, values))
 				return (1);
-			indexes[1]++;
+			j++;
 		}
-		indexes[0]++;
+		i++;
 	}
 	return (0);
 }
@@ -176,19 +185,19 @@ int	solve_stuff(int res[4][4], int given[4][4])
 	This is the main function.
 		argc - number of arguments given by the user in the terminal
 		argv - a list containing these arguments
-		
+
 		Ex. ./a.out Hello There
 				argc = 3
 				argv = {"./a.out", "Hello", "There"}
 			./a.out "Everything in a quotation mark is considered as one"
 				argc = 2
 				argv = {"./a.out", "Everything in a quotation mark is considered as one"}
-	
+
 	Program is executed like => ./a.out "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2"
 	Therefore if the number of argument is greater than 2, it is an error.
-	
+
 	If the user input is also not valid:
-		1. Contains anythign but number
+		1. Contains anything but number
 		2. Does not contain enough number
 	then, it is an error.
 
@@ -198,19 +207,19 @@ int	solve_stuff(int res[4][4], int given[4][4])
 		view[1] -> views from bottom
 		view[2] -> views from left
 		view[3] -> views from right
-	
+
 	we zeroize everything in result[][] first => we make all of its values 0.
-	
+
 	we try to solve the problem using solve_stuff(result, views)
 		if solve_stuff(result, views) returns 0, there was no solution, and it's an error
 		but if it returns 1 => it has successfully solved it.
 	we then print_solution to the terminal
 */
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int			views[4][4];
-	int			result[4][4];
-	int			i;
+	int views[4][4];
+	int result[4][4];
+	int i;
 
 	if (argc != 2 || initialize_array(views, argv[1]) == -1)
 	{
@@ -222,8 +231,8 @@ int	main(int argc, char *argv[])
 		zeroize(result[i++], 4);
 	if (solve_stuff(result, views) == 0)
 	{
-		write(1, "Error\n", 14);
+		write(1, "Error\n", 6);
 		return (0);
 	}
-	print_solution(result, 0, 0, '0');
+	print_solution(result);
 }
--- second.h
+++ second.h
@@ -1,118 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   second.h                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yahaji <yahaji@student.42heilbronn.de>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/12 19:36:57 by yahaji            #+#    #+#             */
-/*   Updated: 2022/02/13 10:31:52 by yahaji           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef FT_SECOND_H
+#define FT_SECOND_H
+int is_col_taken(int n, int col_index);
 
-#include "third.h"
+void zeroize(int *arr, int size);
 
-/*
-* Don't underestimate this function!
-* arguments:
-	1. int n => the number containing the current status of the column
-	2. int col_index => index of column to check.
-* How is it stored?
-	Initially n = 0 => 0000 (in binary form)
-	
-	Example: 
-		when only 0th column is taken then n = 1000 (binary form) = 8
-		when only 1st column is taken then n = 0100 (binary form) = 4
-		when only 2nd column is taken then n = 0010 (binary form) = 2
-		when only 3rd column is taken then n = 0001 (binary form) = 1
-	
-		When 1st and 3rd column is taken n = 0101 (binary form) = 5
-		When 1st, 2nd and 3rd column is taken n = 0111 (binary form) = 7
-		...
-* So if the bit at index of col is 1, then that column is already taken, else that column is still free
-The operators:
-* << = Left Shift -> shifts each bit of a number to the left the specified amount of time.
-	0001 << 2 => 0100
-	0001 << 1 => 0010
-* >> = Right Shift -> shifts each bit of a number to the right the specified amount of time.
-	1100 >> 2 => 0011
-	0101 >> 3 => 0001 (the last one will be removed since it can't go anywhere)
-* & => AND -> if both bits are 1 then 1, else it's 0
-	1010 & 0010 = 0010
-*/
-int	is_col_taken(int n, int col_index)
-{
-	return ((n & (1 << col_index)) >> col_index);
-}
+void set_items(int res[4][4], int *items, int i, int j);
 
-/*
-	Takes an integer array, and makes each of its value 0.
-*/
-void	zeroize(int *arr, int size)
-{
-	while (size--)
-		arr[size] = 0;
-}
+void set_values(int *view_count, int *maxes, int *items);
 
-/*
-	items - contains the value of the table from the four different views
-	items[0] => view from top = res[j][i];
-	items[1] => view from bottom = res[3 - j][i];
-	items[2] => view from left = res[i][3 - j];
-	items[3] => view from right = res[i][3 - j];
+void to_int_array(int *arr, int num);
 
-	so this function puts takes this values from the table (res[4][4])
-	and puts it into the items array.
-*/
-void	set_items(int res[4][4], int *items, int i, int j)
-{
-	items[0] = res[j][i];
-	items[1] = res[3 - j][i];
-	items[2] = res[i][j];
-	items[3] = res[i][3 - j];
-}
-
-/*
-	view_count -> this array contains the number of buildings we can see
-	from all 4 angles (Top, Bottom, Left, Right)
-	maxes -> the height of the maximum building seen from all sides (Top, B, L, R)
-	items - contains the current buildings we are checking from all sides.
-		if the height of the current building (item[i]) is greater than
-		the maximum height of a building from that side (max[i])
-		then we add the number of buildings we can see (view_count[i]++);
-*/
-void	set_values(int *view_count, int *maxes,	int *items)
-{
-	if (items[0] > maxes[0])  // From Top
-	{
-		view_count[0]++;
-		maxes[0] = items[0];
-	}
-	if (items[1] > maxes[1]) // From Bottom
-	{
-		view_count[1]++;
-		maxes[1] = items[1];
-	}
-	if (items[2] > maxes[2]) // From Left
-	{
-		view_count[2]++;
-		maxes[2] = items[2];
-	}
-	if (items[3] > maxes[3]) // From Right
-	{
-		view_count[3]++;
-		maxes[3] = items[3];
-	}
-}
-
-/*
-	changes num to array (list)
-	if num = 4532 => arr becomes {4, 5, 3, 2}
-*/
-void	to_int_array(int *arr, int num)
-{
-	arr[0] = num / 1000;
-	arr[1] = (num / 100) % 10;
-	arr[2] = (num / 10) % 10;
-	arr[3] = num % 10;
-}
+#endif
